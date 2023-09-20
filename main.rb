86bef612811ae85c642d89c10b5918860ab0a322
reckyy <srreki98@gmail.com>
2023-09-20 22:44:03 +0900

    jsonデータを抽出に成功

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
-
+require 'sinatra/reloader'
 require 'sinatra'
+require 'json'
 
 get '/' do
-	@page_title = 'メモアプリ'
+  @page_title = 'メモアプリ'
+	File.open('db.json', 'r') { |io| 
+		@json = JSON.load(io)
+  }
+	erb :index
 end
