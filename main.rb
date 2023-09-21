cea9a940a80d7d1dd3691e6fa11c993c9414ef25
reckyy <srreki98@gmail.com>
2023-09-21 22:40:06 +0900

    新規メモをdb.jsonに保存することに成功

--- main.rb
+++ main.rb
@@ -14,3 +14,14 @@ get '/new' do
   @page_title = 'メモアプリ'
   erb :new
 end
+
+post '/new' do
+  @page_title = 'メモアプリ'
+  old_data_json = File.read('db.json')
+  old_data = JSON.parse(old_data_json)
+  old_data.push({"id" => old_data.size + 1, "title" => params['title'], "content" => params['content']})
+  pushed_data = JSON.dump(old_data)
+  File.write('db.json', pushed_data)
+  redirect '/'
+  erb :index
+end
