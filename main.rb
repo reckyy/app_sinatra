c82914a8c8d197b8d1a197274ca7cf9eefd436df
reckyy <srreki98@gmail.com>
2023-10-08 11:36:12 +0900

    db.jsonの構造をArrayからhashに変更

--- main.rb
+++ main.rb
@@ -13,10 +13,10 @@ def judge_request(json)
   memo_title = CGI.escapeHTML(params['title'])
   memo_content = CGI.escapeHTML(params['content'])
   if request.request_method == 'POST'
-    id = json.size + 1
-    json.push({ 'id' => id, 'title' => memo_title, 'content' => memo_content })
+    id = json.empty? ? 1 : json.keys.last + 1
+    json[id.to_s] = { 'id' => id, 'title' => memo_title, 'content' => memo_content }
   else
-    json[@id - 1] = { 'id' => @id, 'title' => memo_title, 'content' => memo_content }
+    json[@id.to_s] = { 'id' => @id, 'title' => memo_title, 'content' => memo_content }
   end
 end
 
@@ -30,7 +30,7 @@ end
 before do
   @page_title = 'メモアプリ'
   unless File.exist?('db.json')
-    File.write('db.json', '[]')
+    File.write('db.json', '{}')
   end
 end
 
@@ -44,14 +44,14 @@ get '/new' do
 end
 
 get %r{/(\d+)} do |id|
-  @id = id.to_i
+  @id = id
   @json = read_json
   erb :show
 end
 
 get %r{/(\d+)/edit} do |id|
   @page_title = 'メモ編集'
-  @id = id.to_i
+  @id = id
   @json = read_json
   erb :edit
 end
@@ -62,15 +62,15 @@ post '/new' do
 end
 
 patch %r{/(\d+)/edit} do |id|
-  @id = id.to_i
+  @id = id
   edit_db
   redirect "/#{@id}"
 end
 
 delete %r{/(\d+)/edit} do |id|
-  @id = id.to_i
+  @id = id
   old_data = read_json
-  old_data.delete_at(@id - 1)
+  old_data.delete(@id)
   new_data = JSON.dump(old_data)
   File.write('db.json', new_data)
   redirect '/'
